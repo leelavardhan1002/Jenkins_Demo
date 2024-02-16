65f3576fc46715896e925bd52ffcc11d9858dcb0
Leelavardhan <leelavardhan9@gmail.com>
2024-02-16 12:25:28 +0530

    Initial Push

---
+++ src/test/java/com/training/studentservice/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.studentservice;
+
+import org.junit.jupiter.api.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
